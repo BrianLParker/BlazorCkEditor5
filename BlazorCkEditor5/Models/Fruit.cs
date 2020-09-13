070fa11735d70bce93ff8b45c726fbd255267ecf
Brian Parker <brian.parker@hotmail.com.au>
2020-09-13 20:57:52 +1000

    Add project files.

---
+++ BlazorCkEditor5/Models/Fruit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCkEditor5.Models
+{
+    public class Fruit
+    {
+        public string Description { get; set; }
+    }
+}
